7fd387398a07593a739811cc1844a72680154db1
zennerleet <rrazvanftw@gmail.com>
2020-11-05 15:08:01 +0200

    Added .hpp

---
+++ proj1/Type_Define.hpp
@@ -0,0 +1,108 @@
+
+#ifndef TYPE_DEFINE_H
+#define TYPE_DEFINE_H
+
+
+class Pasager  {
+    private:
+        string numePasager;
+        string clasa;//economic,business,intai
+    public:
+        Pasager(string numePasager, string clasa);
+
+        ~Pasager() {}
+        void print() {
+             cout << numePasager << " la clasa " << clasa;
+        }
+};
+
+
+Pasager::Pasager(string numePasager, string clasa) :
+numePasager(numePasager),
+clasa(clasa) { cout << "Tocmai am creat pasagerul " << numePasager <<endl;}
+
+
+
+class Avion {
+    public:
+        string idAvion;
+        int putereMotor;
+        Avion(string idAvion, int putereMotor):
+        idAvion(idAvion),
+        putereMotor(putereMotor) {}
+        
+        virtual string toString() {
+            return idAvion + " " + to_string(putereMotor) + "\n";
+        }
+
+        void decolare(){
+            cout<< "Avionul " + idAvion + "a decolat.\n";
+        }
+        void aterizare(){
+            cout<< "Avionul " + idAvion + "a aterizat.\n";
+        }
+
+        ~Avion() { }
+};
+
+class AvionDeCalatori : public Avion{
+    
+    public:
+        int nrMaxPasageri;
+        vector<Pasager> pasageri;
+        AvionDeCalatori(string idAvion, int putereMotor) :
+        Avion::Avion(idAvion, putereMotor),
+        
+        nrMaxPasageri(12){}
+
+        void adaugaPasageri(Pasager p){
+            if(nrMaxPasageri > 0){
+                pasageri.push_back(p);
+                cout<<"Pasagerul ";
+                p.print(); 
+                cout<<" a fost imbarcat in " << idAvion << "\n";
+                nrMaxPasageri--;
+            }
+            else
+            {
+                cout<< "Avionul este plin!\n";
+            }
+        }
+        void afisarePasageri(){
+            cout<< "Pasageri imbarcati in " << idAvion << ":\n";
+            for(Pasager p: pasageri){
+                cout << "\t - ";
+                p.print();
+                cout << endl;
+            }
+            cout << "Locuri ramase: " << nrMaxPasageri << endl;
+        }
+};
+
+class AvionDeLupta : public Avion {
+    public:
+        int nrRachete;
+        AvionDeLupta(string idAvion, int putereMotor) :
+        Avion::Avion(idAvion, putereMotor),
+        nrRachete(2) {}
+
+        string toString() {
+            string buf = Avion::toString() + "\n" + to_string(nrRachete) + "\n";
+            return buf;
+        }
+        void lanseazaRacheta(){
+            if(nrRachete >0){
+                cout<< "Avionul " + idAvion + "a lansat racheta\n";
+                nrRachete--;
+            }
+            else
+            {
+                cout<<"Nu mai sunt rachete!" << endl;
+            }
+            
+        }
+};
+
+
+
+#endif
--- proj1/main.cpp
+++ proj1/main.cpp
@@ -4,110 +4,21 @@
 #include <vector>
 using namespace std;
 
-class Pasager {
-    private:
-        string numePasager;
-        string clasa;//economic,business,intai
-    public:
-        Pasager(string numePasager, string clasa);
 
-        ~Pasager() {
-            cout << "\nAm omorat pasagerul " << numePasager;
-        }
-        void print() {
-             cout << numePasager;
-        }
-};
+#include "Type_Define.hpp"
 
-Pasager::Pasager(string numePasager, string clasa) :
-numePasager(numePasager),
-clasa(clasa) { cout << "Tocmai am creat pasagerul " << numePasager <<endl;}
-
-class Avion {
-    public:
-        string idAvion;
-        int putereMotor;
-        Avion(string idAvion, int putereMotor):
-        idAvion(idAvion),
-        putereMotor(putereMotor) {}
-        
-        string toString() {
-            return idAvion + " " + to_string(putereMotor);
-        }
-
-        void decolare(){
-            cout<< "Avionul " + idAvion + "a decolat./n";
-        }
-        void zboara(){
-            cout<< "Avionul " + idAvion + " zboara./n";
-        }
-        void aterizare(){
-            cout<< "Avionul " + idAvion + "a aterizat./n";
-        }
-};
-
-class AvionDeCalatori : public Avion{
-    public:
-        int nrMaxPasageri;
-        vector<Pasager> pasageri;
-        AvionDeCalatori(string idAvion, int putereMotor) :
-        Avion::Avion(idAvion, putereMotor),
-        
-        nrMaxPasageri(12){}
-
-        void adaugaPasageri(Pasager p){
-            if(nrMaxPasageri > 0){
-                pasageri.push_back(p);
-                cout<<"Pasagerul ";
-                p.print(); 
-                cout<<" a fost imbarcat.\n";
-                nrMaxPasageri--;
-            }
-            else
-            {
-                cout<< "Avionul este plin!";
-            }
-        }
-        void afisarePasageri(){
-            cout<< "Pasageri imbarcati:/n";
-            for(Pasager p: pasageri){
-                p.print();
-            }
-        }
-};
-
-class AvionDeLupta : public Avion {
-    public:
-        int nrRachete;
-        AvionDeLupta(string idAvion, int putereMotor) :
-        Avion::Avion(idAvion, putereMotor),
-        nrRachete(10) {}
-
-        string toString() {
-            string buf = Avion::toString() + "\n" + to_string(nrRachete);
-            return buf;
-        }
-        void lanseazaRacheta(){
-            if(nrRachete >0){
-                cout<< "Avionul " + idAvion + "a lansat racheta";
-                nrRachete--;
-            }
-            else
-            {
-                cout<<"Nu mai sunt rachete!";
-            }
-            
-        }
-};
 
 int main() {
 
-    AvionDeCalatori avionDeCalatori1("aaa",2500);
+    AvionDeCalatori avionDeCalatori1("Boeing777",2500);
     AvionDeCalatori avionDeCalatori2("bbb",3000);
     AvionDeCalatori avionDeCalatori3("ccc",3000);
 
     AvionDeLupta avionDeLupta1("ddd",3500);
     AvionDeLupta avionDeLupta2("eee",4500);
+
+    Avion *a2 = new AvionDeLupta("lupta", 5);
+    cout << a2->toString();
 
     Pasager p1("miruna","intai");
     Pasager p2("alex","economic");
@@ -118,6 +29,18 @@ int main() {
     avionDeCalatori1.adaugaPasageri(p1);
     avionDeCalatori1.adaugaPasageri(p3);
     avionDeCalatori1.adaugaPasageri(p4);
+
+    avionDeCalatori1.afisarePasageri();
+
+
+    avionDeCalatori1.adaugaPasageri(p2);
+    avionDeCalatori1.afisarePasageri();
+
+    avionDeLupta1.decolare();
+    do {
+        avionDeLupta1.lanseazaRacheta();
+    } while (avionDeLupta1.nrRachete > 0);
+    avionDeLupta1.aterizare();
 
     return 0;
 }
